b8df6ec645cee05c4fb0fd9c0d3c3f268c3a238a
Kris Szafranski <kdszafranski@gmail.com>
2017-07-28 16:10:12 -0500

    fixed json error. i am sorry

--- data.js
+++ data.js
@@ -21,5 +21,5 @@ var peopleArray = [
   {name: "Eric Austin", shoutout: "Sabrina" },
   {name: "Kevin McMahon", shoutout: "Shout out to my friend Susan pushing me to stop being content exactly 1 year ago on 7/24/16" },
   {name: "Luke", shoutout: "Amazing speeches this morning everybody! Huge ups for the self-proclaimed nervous among you. You did great work!" },
-  {name: "Brendt Bly", shoutout: "Shout out to Adam for the term "Grenade in a box!" which I will now be using daily." }
+  {name: "Brendt Bly", shoutout: "Shout out to Adam for the term 'Grenade in a box!' which I will now be using daily." }
 ];
