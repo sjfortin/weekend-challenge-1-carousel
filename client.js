01318331ed2afdad1881a750dd478ec7b9599322
Sam Fortin <sam.j.fortin@gmail.com>
2017-07-29 10:43:52 -0500

    First person/shoutout showing on doc ready

--- client.js
+++ client.js
@@ -1,7 +1,6 @@
 /*
 To Dos:
 - One person at a time should be shown on the DOM represented by showing their name and their shout out
-- Include "Prev" and "Next" buttons
 - clicking "Prev" when on the first person should wrap around to show the last person and vice versa.
 - DOM should display showing the number of people and which is being currently viewed (eg. 2/20).
 - When a person is displayed, show their first name, last name and their shout out. Only one person should be shown at any given time.
@@ -9,12 +8,16 @@ To Dos:
 - Include a timer that moves to the next person if the user is not clicking on "Next" or "Prev". If the user clicks on "Next" or "Prev", the timer should be reset. The timer should transition between people every 10 seconds.
 
 Complete:
-
+- Include "Prev" and "Next" buttons
 
 */
 
 console.log('peopleArray from data.js: ', peopleArray);
 
 $(document).ready(function () {
-    
+    var currentPerson = 1;
+
+    $('#name').html(peopleArray[0].name);
+    $('#shoutout').html(peopleArray[0].shoutout);
+    $('#current-person').html(currentPerson + "/23");
 });
