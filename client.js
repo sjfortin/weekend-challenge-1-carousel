33f34f5fc93c7bb179c335b7a977184c568824ac
Sam Fortin <sam.j.fortin@gmail.com>
2017-07-29 10:24:18 -0500

    Added To Do Comments to client.js

--- client.js
+++ client.js
@@ -1,6 +1,20 @@
+/*
+To Dos:
+- One person at a time should be shown on the DOM represented by showing their name and their shout out
+- Include "Prev" and "Next" buttons
+- clicking "Prev" when on the first person should wrap around to show the last person and vice versa.
+- DOM should display showing the number of people and which is being currently viewed (eg. 2/20).
+- When a person is displayed, show their first name, last name and their shout out. Only one person should be shown at any given time.
+- Include a fade out and fade in animation in-between transitioning people.
+- Include a timer that moves to the next person if the user is not clicking on "Next" or "Prev". If the user clicks on "Next" or "Prev", the timer should be reset. The timer should transition between people every 10 seconds.
+
+Complete:
+
+
+*/
+
 console.log('peopleArray from data.js: ', peopleArray);
 
 $(document).ready(function () {
-    console.log('DOM is loaded');
     
 });
