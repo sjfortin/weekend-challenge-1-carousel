577f68591bce095ab59d83714e48e0997a51a6a7
Sam Fortin <sam.j.fortin@gmail.com>
2017-07-29 11:37:44 -0500

    Updated quotes to be consistent with single quotes. Added 0 to single digits in the current person display

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
 /*
 To Dos:
-- Include a timer that moves to the next person if the user is not clicking on "Next" or "Prev". If the user clicks on "Next" or "Prev", the timer should be reset. The timer should transition between people every 10 seconds.
+- Include a timer that moves to the next person if the user is not clicking on 'Next' or 'Prev'. If the user clicks on 'Next' or 'Prev', the timer should be reset. The timer should transition between people every 10 seconds.
 
 Complete:
-- Include "Prev" and "Next" buttons
+- Include 'Prev' and 'Next' buttons
 - One person at a time should be shown on the DOM represented by showing their name and their shout out
-- clicking "Prev" when on the first person should wrap around to show the last person and vice versa.
+- clicking 'Prev' when on the first person should wrap around to show the last person and vice versa.
 - DOM should display showing the number of people and which is being currently viewed (eg. 2/20).
 - When a person is displayed, show their first name, last name and their shout out. Only one person should be shown at any given time.
 - Include a fade out and fade in animation in-between transitioning people.
@@ -20,7 +20,7 @@ $(document).ready(function () {
     // Initial View on DOM load
     $('#name').html(peopleArray[currentPerson-1].name);
     $('#shoutout').html(peopleArray[currentPerson - 1].shoutout);
-    $('#current-person').html(currentPerson + " out of 23");
+    $('#current-person').html('0' + currentPerson + ' out of 23');
 
     // Add event listeners for Next and Previous Buttons
     $('.buttons').on('click', '#next', nextPerson);
@@ -48,8 +48,12 @@ $(document).ready(function () {
 
     // Function to display the current person when next or previous button is clicked
     function displayCurrentPerson() {
-        $('#name').hide().html(peopleArray[currentPerson - 1].name).fadeIn('slow');
-        $('#shoutout').hide().html(peopleArray[currentPerson - 1].shoutout).fadeIn('slow');
-        $('#current-person').html(currentPerson + " out of 23");
+        $('#name').hide().html(peopleArray[currentPerson - 1].name).fadeIn('fast');
+        $('#shoutout').hide().html(peopleArray[currentPerson - 1].shoutout).fadeIn('fast');
+        if(currentPerson < 10) {
+            $('#current-person').html('0' + currentPerson + ' out of 23');
+        } else {
+            $('#current-person').html(currentPerson + ' out of 23');
+        }
     }
 });
